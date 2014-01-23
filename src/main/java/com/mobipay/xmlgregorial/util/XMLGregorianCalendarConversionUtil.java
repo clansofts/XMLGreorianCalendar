dde69ecdcae5c0f0c85c1b5585ec49a22b1c9a7b
clansofts <clansofts@localhost.localdomain>
2014-01-23 12:28:19 +0300

    Initial XML Gregorian Calendar Converter Commit Thursday 23rd Jan 2014 11:40 am.

---
+++ src/main/java/com/mobipay/xmlgregorial/util/XMLGregorianCalendarConversionUtil.java
@@ -0,0 +1,60 @@
+package com.mobipay.xmlgregorial.util;
+
+import java.util.GregorianCalendar;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+/**
+ *
+ * @author PRINCE
+ */
+/**
+ * A utility class for converting objects between java.util.Date and
+ * XMLGregorianCalendar types
+ * 
+*/
+public class XMLGregorianCalendarConversionUtil {
+
+// DatatypeFactory creates new javax.xml.datatype Objects that map XML
+// to/from Java Objects.
+    private static DatatypeFactory df = null;
+
+    static {
+        try {
+            df = DatatypeFactory.newInstance();
+        } catch (DatatypeConfigurationException e) {
+            throw new IllegalStateException(
+                    "Error while trying to obtain a new instance of DatatypeFactory", e);
+        }
+    }
+
+    /**
+     *
+     * @param date
+     * @return Converts a java.util.Date into an instance of
+     * XMLGregorianCalendar
+     */
+    public static XMLGregorianCalendar asXMLGregorianCalendar(java.util.Date date) {
+        if (date == null) {
+            return null;
+        } else {
+            GregorianCalendar gc = new GregorianCalendar();
+            gc.setTimeInMillis(date.getTime());
+            return df.newXMLGregorianCalendar(gc);
+        }
+    }
+
+    /**
+     *
+     * @param xmlGC
+     * @return Converts an XMLGregorianCalendar to an instance of java.util.Date
+     */
+    public static java.util.Date asDate(XMLGregorianCalendar xmlGC) {
+        if (xmlGC == null) {
+            return null;
+        } else {
+            return xmlGC.toGregorianCalendar().getTime();
+        }
+    }
+}
